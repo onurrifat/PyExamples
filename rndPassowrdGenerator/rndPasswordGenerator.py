39b16a6ea6b74035b7de0133562b7e7282fe0c7b
onurrifat <72670432+onurrifat@users.noreply.github.com>
2021-01-22 14:48:41 +0300

    Add files via upload

---
+++ rndPassowrdGenerator/rndPasswordGenerator.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+def password_generator(size=15, chars=(string.ascii_uppercase + string.ascii_lowercase+string.digits)):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+#change 'size' for password length
+
+print(password_generator())
+
+
+
+
+
+
